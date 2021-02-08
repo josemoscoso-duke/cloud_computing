ae6d71aabb6d55816290d1b0c6db814abf1030ae
Jose Moscoso <jose.moscoso@duke.edu>
2021-02-08 15:49:06 +0000

    Update (README.md, weather_app.py) and add (app.yaml, config_weather.py, weather.ini)files in repository

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cloud_computing
-Cloud computing at scale - IDS721 projects
+# Weatherapp using Flask-App-on-GCP
+This project is an implementation of the  continuous delivery framework based on the guidelines of the Cloud computing at scale class @ Duke University. 
+
+## Overview
+I use an API of the Openweathermap [openweathermap](https://openweathermap.org/). This application uses Flask in python for interacting with the openweathermap API. By indicating the city of interest as an input, the app makes a call to Openweathermap using the api and the request library, and returns weather conditions on real time at the city  (temperature, wind speed and cloud conditions). The output of the Openweathermap is then sent over via flask on a html web page.
+
+For the purpose of continous delivery, Cloud Build on Google Cloud Platform (GCP) is used. The deployment is trigerred whenver a push is made on the the repository. 
+
---
+++ weather_app.py
@@ -0,0 +1,38 @@
+import requests
+from flask import Flask, request
+from config_weather import config
+
+APPID = config()['appid'] # initialize weather api here
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    city=request.args.get("city", "")
+    if city:
+        weather_report=weather_data(city)
+    else:
+        weather_report='please indicate a correct city name'
+    return ( 
+        """<form action="" method="get">
+                <h1>Welcome to my weather app</h1>
+                city: <input type="text" name="city">
+                <input type="submit" value="get weather report">
+            </form>"""
+            + "Weather report: "
+            + weather_report
+        )
+
+def weather_data(city):
+    res=requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={APPID}&units=metric")
+    result = res.json()
+    if result.get('cod') != 200:
+        return f'Error getting temperature for {city}. Check the city name'
+    else:
+        return ("{}'s temperature: {}°C   ".format(city,result['main']['temp']) + 
+            "Wind speed: {} m/s   ".format(result['wind']['speed']) + 
+	        "Description: {}   ".format(result['weather'][0]['description']) +
+	        "Weather: {}".format(result['weather'][0]['main']))
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
